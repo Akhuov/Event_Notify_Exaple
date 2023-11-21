d1146507bfb360fac95ed61b194e0a11ad1d9a95
user <user@WIN-F7NIMF7A3VO>
2023-11-21 07:27:04 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+Class Func = new Class(100);
+Func.Notify += DisplayMessage;
+Func.Summa(70);
+
+
+
+void DisplayMessage(string message) => Console.WriteLine(message);
+
+
+public class Class
+{
+    public delegate void Handler(string message);
+    public event Handler? Notify;
+    public Class(int sum) => Sum = sum;
+    public int Sum { get; set; }
+    public void Summa(int sum)
+    {
+        Sum += sum;
+        Notify?.Invoke($"Со счета снято: {sum}"); 
+    }
+}
